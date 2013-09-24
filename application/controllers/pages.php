ce0546f708aaf720e71253354e7364479f979ed4
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-24 14:05:54 +0700

    - mengisi method view pada class(application/controllers/pages) - dan membetulkan beberap source code yang tidak rapi

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -20,7 +20,16 @@ class pages extends CI_Controller {
     }
 
     public function view($page = 'home') {
-        
+        if (!file_exists('application/views/pages/' . $page . '.php')) {
+            //Whooppss, we don't have a page for that
+            show_404();
+        } else {
+            $data['title'] = ucfirst($page); //Auto Capitalize the first letter
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/' . $page, $data);
+            $this->load->view('templates/footer', $data);
+        }
     }
 
 }
