c128aa19aae833799054e9a8815e8694bd93a1b4
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-24 10:53:03 +0700

    static pages, add class pages to controller

---
+++ application/controllers/pages.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of pages
+ *
+ * @author HANDITO
+ */
+class pages extends CI_Controller {
+
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function view($page = 'home') {
+        
+    }
+
+}
+
+/* End of file pages.php */
+/* Location: ./application/controllers/pages.php */
