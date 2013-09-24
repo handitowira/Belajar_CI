fbef0d28de8fbdf21e2ccd6028eedb83bece1acc
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-24 10:56:13 +0700

    membagi view menjadi 3 bagian, pengerjaan masih pada footer dan header

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,14 +3,6 @@ To change this template, choose Tools | Templates
 and open the template in the editor.
 -->
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<strong>&copy; 2013 Kurniawan Wira Handito</strong>
+</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,12 +5,7 @@ and open the template in the editor.
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title><?php echo $title ?> - Static Pages | Belajar CI</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+        <h1>Static Pages | Belajar CI</h1>
