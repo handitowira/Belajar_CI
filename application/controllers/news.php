3874f1fad51ec0ac4a8f83749b10db952f7c1919
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 11:50:24 +0700

    create function for check whether the form was submitted and whether the submitted data passed the validation rules

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -45,6 +45,25 @@ class news extends CI_Controller {
         $this->load->view('templates/footer');
     }
 
+    public function create() {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $data['title'] = 'Create a news item';
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'Text', 'required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('news/create');
+            $this->load->view('templates/foter');
+        } else {
+            $this->news_model->set_news();
+            $this->load->view('news/success');
+        }
+    }
+
 }
 
 /* End of file news.php */
