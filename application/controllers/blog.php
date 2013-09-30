65b9056725a64466279189cc66e92519c9ab6b08
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 15:08:32 +0700

    menambah standarisasi ke setiap kelas

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,15 +14,18 @@
  *
  * @author HANDITO
  */
-class blog extends CI_Controller {
+class Blog extends CI_Controller {
 
     //put your code here
     public function __construct() {
         parent::__construct();
     }
-    
-    public function index(){
+
+    public function index() {
         echo 'Hallo CI';
     }
 
 }
+
+/* End of file blog.php */
+/* Location: ./application/controllesr/blog.php */
