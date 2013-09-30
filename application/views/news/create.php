e6b7256b282dcd02a52404dc20f102bea0722149
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 12:04:32 +0700

    fixing some ...........

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -7,14 +7,14 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Create | News</title>
+        <title></title>
     </head>
     <body>
         <h2>Create a news item</h2>
         <?php echo validation_errors(); ?>
         <?php echo form_open('news/create'); ?>
         <label for="title">Title</label>
-        <input type="input" name="title" required=""/><br/>
+        <input type="input" name="title"/><br/>
         <label for="text">Text</label>
         <textarea name="text"></textarea><br/>
         <input type="submit" name="Submit" value="Create new items"/>
