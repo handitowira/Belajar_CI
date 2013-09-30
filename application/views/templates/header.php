6c55a70aca92c629d1b1325d9c21d4f6ad07f318
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 12:05:02 +0700

    just add '|'

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@ and open the template in the editor.
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo $title ?> Belajar CI</title>
+        <title><?php echo $title ?> | Belajar CI</title>
     </head>
     <body>
         <h1>Static Pages | Belajar CI</h1>
