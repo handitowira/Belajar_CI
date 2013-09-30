1636dee522fadac94f28ab0eb8d77b5053130907
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 15:44:21 +0700

    passing data from controllers/news

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -4,12 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>Welcome to my blog!</h1>
-    </body>
-</html>
+<h1>Welcome to my blog!</h1>
+<h3>
+    <?php echo $message; ?>
+</h3>
