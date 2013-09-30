00974d82a0a9f91454f5fb4241bc9f6a2d2aa59a
Kurniawan Wira Handito <handito.kw@gmail.com>
2013-09-30 11:39:13 +0700

    create a form where you can input the information to be stored. This means you'll be needing a form with two fields, one for the title and one for the text. You'll derive the slug from our title in the model.

---
+++ application/views/news/create.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Create | News</title>
+    </head>
+    <body>
+        <h2>Create a news item</h2>
+        <?php echo validation_errors(); ?>
+        <?php echo form_open('news/create'); ?>
+        <label for="title">Title</label>
+        <input type="input" name="title" required=""/><br/>
+        <label for="text">Text</label>
+        <textarea name="text"></textarea><br/>
+        <input type="submit" name="Submit" value="Create new items"/>
+        <?php echo form_close(); ?>
+    </body>
+</html>
